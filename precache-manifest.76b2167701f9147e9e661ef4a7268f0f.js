c3b6737a2baadfc9664dd99fd3b2a033b1c3176e
[Aizat Borhanudin] <[aizat@invokeisdata.com]>
2020-09-05 03:32:23 +0800

    Updates

---
+++ precache-manifest.76b2167701f9147e9e661ef4a7268f0f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2010016c250a5b54b188796b4acc8929",
+    "url": "/BREW_team_5/index.html"
+  },
+  {
+    "revision": "de63a5009943c1c86596",
+    "url": "/BREW_team_5/static/css/2.4df217b3.chunk.css"
+  },
+  {
+    "revision": "0c305b61c2fbc6795da1",
+    "url": "/BREW_team_5/static/css/main.277d8e66.chunk.css"
+  },
+  {
+    "revision": "de63a5009943c1c86596",
+    "url": "/BREW_team_5/static/js/2.5e6331d4.chunk.js"
+  },
+  {
+    "revision": "ae5514eb1e4e0cc97b3500adac3ad9a8",
+    "url": "/BREW_team_5/static/js/2.5e6331d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c305b61c2fbc6795da1",
+    "url": "/BREW_team_5/static/js/main.df8b132b.chunk.js"
+  },
+  {
+    "revision": "8ba6fb8c4e0cffd389b2",
+    "url": "/BREW_team_5/static/js/runtime-main.c454a52f.js"
+  },
+  {
+    "revision": "26e08a9ee1c06a560220c4902158dfba",
+    "url": "/BREW_team_5/static/media/affiq.26e08a9e.png"
+  },
+  {
+    "revision": "433651c9e3f163feb0caa0583e4011f3",
+    "url": "/BREW_team_5/static/media/aizat.433651c9.png"
+  },
+  {
+    "revision": "1cf966bad5392d7590862a6f5805cf16",
+    "url": "/BREW_team_5/static/media/aliya.1cf966ba.png"
+  },
+  {
+    "revision": "d606b059aaebb9f737a05880202a683d",
+    "url": "/BREW_team_5/static/media/banner1.d606b059.jpg"
+  },
+  {
+    "revision": "29c16612ce026fef375fc7e01e953929",
+    "url": "/BREW_team_5/static/media/buddy.29c16612.png"
+  },
+  {
+    "revision": "92c317bd0eb13bc172e8a69a7734f71e",
+    "url": "/BREW_team_5/static/media/fitri.92c317bd.png"
+  },
+  {
+    "revision": "e266d5a5ad6ab361a7b74e5b8f5ce19f",
+    "url": "/BREW_team_5/static/media/frontpage.e266d5a5.jpg"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/BREW_team_5/static/media/google-play-badge.db9b21a1.png"
+  },
+  {
+    "revision": "875e76b32d4a2b54aab9b8838f7107dc",
+    "url": "/BREW_team_5/static/media/login.875e76b3.png"
+  },
+  {
+    "revision": "4bbf1db518995635e3c62f7c7c2ad209",
+    "url": "/BREW_team_5/static/media/lukman.4bbf1db5.jpg"
+  },
+  {
+    "revision": "dad0f7f292019da1a88ce8a9f7f9e386",
+    "url": "/BREW_team_5/static/media/mobile.dad0f7f2.png"
+  },
+  {
+    "revision": "6ce3570c57455d654e06f1c5ba30e1c2",
+    "url": "/BREW_team_5/static/media/organize.6ce3570c.png"
+  },
+  {
+    "revision": "1e7a12ecd7315820e0294ab492d65bcf",
+    "url": "/BREW_team_5/static/media/register.1e7a12ec.png"
+  },
+  {
+    "revision": "a95f2c03bdd8fa3be498a5f2e201c54a",
+    "url": "/BREW_team_5/static/media/shakti.a95f2c03.png"
+  },
+  {
+    "revision": "834659b8eedd8a20770bbc876874a41a",
+    "url": "/BREW_team_5/static/media/shamiza.834659b8.png"
+  },
+  {
+    "revision": "a3bcab975f08449e8c1be946da51133e",
+    "url": "/BREW_team_5/static/media/sharafi.a3bcab97.png"
+  },
+  {
+    "revision": "3ee2a25226886bafb8cf479e663d91e4",
+    "url": "/BREW_team_5/static/media/siti.3ee2a252.png"
+  },
+  {
+    "revision": "eedf5f536023741a6245e79119cacc4e",
+    "url": "/BREW_team_5/static/media/vlogo.eedf5f53.png"
+  }
+]);
